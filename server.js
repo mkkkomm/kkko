8c18d1a214a188bc9e9495bca26f41ac538f1a64
mkkkomm <154680685+mkkkomm@users.noreply.github.com>
2023-12-23 15:34:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post('/send-message', async (req, res) => {
     const { message } = req.body;
 
     // Replace 'YOUR_DISCORD_WEBHOOK_URL' with your actual Discord webhook URL
-    const webhookUrl = 'YOUR_DISCORD_WEBHOOK_URL';
+    const webhookUrl = 'https://discord.com/api/webhooks/1188112391813206047/iF7W_epbO9ZI79xDq1ohINLHESmWGeVmifie7KX4pfN8FyWwIZ7xdxWOuDQLphBevvTJ';
 
     await fetch(webhookUrl, {
         method: 'POST',
