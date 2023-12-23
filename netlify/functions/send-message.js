ee03cfea39c8d3955635b99f8f75e3b4f845d3a9
mkkkomm <154680685+mkkkomm@users.noreply.github.com>
2023-12-23 15:54:22 +0200

    Update send-message.js

--- netlify/functions/send-message.js
+++ netlify/functions/send-message.js
@@ -1,1 +1,24 @@
+// netlify/functions/send-message.js
+
+exports.handler = async function (event, context) {
+    const { message } = JSON.parse(event.body);
+
+    // Replace 'YOUR_DISCORD_WEBHOOK_URL' with your actual Discord webhook URL
+    const webhookUrl = 'https://discord.com/api/webhooks/1188112391813206047/iF7W_epbO9ZI79xDq1ohINLHESmWGeVmifie7KX4pfN8FyWwIZ7xdxWOuDQLphBevvTJ';
+
+    await fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            content: message,
+        }),
+    });
+
+    return {
+        statusCode: 200,
+        body: 'Message sent to Discord!',
+    };
+};
 
